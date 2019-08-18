397a66f1afaf7e4e5f266f5f97630ef8162cde58
cycloawaodorin <cycloawaodorin+github@gmail.com>
2019-08-18 19:01:51 +0900

    use wrapped function to draw triangle

--- scripts/ksa.lua
+++ scripts/ksa.lua
@@ -4,10 +4,20 @@ ksa.drawrect = function(obj, x0, y0, x1, y1, alpha, u0, v0, u1, v1)
 	if ( u0 ) then
 		obj.drawpoly(x0,y0,0, x1,y0,0, x1,y1,0, x0,y1,0, u0,v0, u1,v0, u1,v1, u0,v1, alpha)
 	elseif ( alpha ) then
-		u0, v0, u1, v1 = 0, 0, obj.w, obj.h
+		local u0, v0, u1, v1 = 0, 0, obj.w, obj.h
 		obj.drawpoly(x0,y0,0, x1,y0,0, x1,y1,0, x0,y1,0, u0,v0, u1,v0, u1,v1, u0,v1, alpha)
 	else
 		obj.drawpoly(x0,y0,0, x1,y0,0, x1,y1,0, x0,y1,0)
+	end
+end
+
+ksa.drawtri = function(obj, x0, y0, x1, y1, x2, y2, alpha)
+	local mid_x, mid_y = (x1+x2)/2, (y1+y2)/2
+	if ( alpha ) then
+		local u0, v0, u1, v1 = 0, 0, obj.w, obj.h
+		obj.drawpoly(x0,y0,0, x1,y1,0, mid_x,mid_y,0, x2,y2,0, u0,v0, u1,v0, u1,v1, u0,v1, alpha)
+	else
+		obj.drawpoly(x0,y0,0, x1,y1,0, mid_x,mid_y,0, x2,y2,0)
 	end
 end
 
