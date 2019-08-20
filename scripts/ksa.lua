0488aff5659c9d25522d45a7bd5e682402a74246
cycloawaodorin <cycloawaodorin+github@gmail.com>
2019-08-21 02:04:00 +0900

    use global obj in ksa.lua

--- scripts/ksa.lua
+++ scripts/ksa.lua
@@ -1,6 +1,6 @@
 local ksa = {}
 
-ksa.drawrect = function(obj, x0, y0, x1, y1, alpha, u0, v0, u1, v1)
+ksa.drawrect = function(x0, y0, x1, y1, alpha, u0, v0, u1, v1)
 	if ( u0 ) then
 		obj.drawpoly(x0,y0,0, x1,y0,0, x1,y1,0, x0,y1,0, u0,v0, u1,v0, u1,v1, u0,v1, alpha)
 	elseif ( alpha ) then
@@ -11,7 +11,7 @@ ksa.drawrect = function(obj, x0, y0, x1, y1, alpha, u0, v0, u1, v1)
 	end
 end
 
-ksa.drawtri = function(obj, x0, y0, x1, y1, x2, y2, alpha)
+ksa.drawtri = function(x0, y0, x1, y1, x2, y2, alpha)
 	local mid_x, mid_y = (x1+x2)/2, (y1+y2)/2
 	if ( alpha ) then
 		local u0, v0, u1, v1 = 0, 0, obj.w, obj.h
