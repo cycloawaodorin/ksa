65b437d673bc5b888ba2e3172211f951e29b0e0a
cycloawaodorin <cycloawaodorin+github@gmail.com>
2019-08-21 22:17:56 +0900

    fix to dirty global namespace

--- src/ksa_ext.cpp
+++ src/ksa_ext.cpp
@@ -31,8 +31,8 @@ ksa_trsgrad(lua_State *L)
 	// 引数受け取り
 	int i=0;
 	PIXEL_BGRA *data = static_cast<PIXEL_BGRA *>(lua_touserdata(L, ++i));
-	int w = lua_tointeger(L, ++i);
-	int h = lua_tointeger(L, ++i);
+	int w = static_cast<int>(lua_tointeger(L, ++i));
+	int h = static_cast<int>(lua_tointeger(L, ++i));
 	TRSGRAD_PARAM p;
 	p.cx = static_cast<float>(lua_tonumber(L, ++i));
 	p.cy = static_cast<float>(lua_tonumber(L, ++i));
@@ -67,7 +67,7 @@ extern "C" {
 __declspec(dllexport) int
 luaopen_ksa_ext(lua_State *L)
 {
-	luaL_register(L, nullptr, ksa_ext);
+	luaL_register(L, "ksa_ext", ksa_ext);
 	return 1;
 }
 }
